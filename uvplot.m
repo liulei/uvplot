851d796ec37c0a940b4d6fe2a83914c33db92e13
Lei Liu <liulei@shao.ac.cn>
2014-09-27 00:16:31 +0800

    Program that gives the correct dirty image.

--- uvplot.m
+++ uvplot.m
@@ -2,9 +2,9 @@ function uvplot()
 % plot (l,m) figure
 %
 
-ng = 512;
+ng = 256;
 
-target = 'ein';
+target = 'bk';
 uvname = strcat(target, '.uv');
 pngname = strcat(target, '_dirt_', num2str(ng), '.png');
 
@@ -26,6 +26,8 @@ weight = arr(:, offset + 3);
 mu = max(u);
 mv = max(v);
 maxuv = max(mu, mv) * 1.00001;
+
+maxuv = maxuv * 2;
 
 fsize = 17;
 figure(1);
@@ -55,37 +57,52 @@ beamarr_r = zeros(ng, ng);
 beamarr_c = zeros(ng, ng);
 beamarr = complex(beamarr_r, beamarr_c);
 
-idu = floor((u - uleft) / du) + 1;
-idv = floor((v - vleft) / dv) + 1;
+%idu = floor((u - uleft) / du) + 1;
+%idv = floor((v - vleft) / dv) + 1;
 
-for i=1:length(idu)
-    visarr(idv(i), idu(i)) = visarr(idv(i), idu(i)) + vis(i);
-    beamarr(idv(i), idu(i)) = 1.0;
+for i=1:length(u)
+    
+    idu = floor(u(i) / du);
+    if(idu < 0)
+        idu = idu + ng;
+    end
+    idu = idu + 1;
+    
+    idv = floor(v(i) / dv);
+    if(idv < 0)
+        idv = idv + ng;
+    end
+    idv = idv + 1;
+    
+    visarr(idv, idu) = visarr(idv, idu) + vis(i);
+    beamarr(idv, idu) = 1.0;
 end
 
-figure(2);
-imagesc(abs(visarr));
-colormap(jet);
-colorbar();
+%figure(2);
+%imagesc(abs(visarr));
+%colormap(jet);
+%colorbar();
 
-figure(3);
-imagesc(abs(beamarr));
-colormap(jet);
-colorbar();
+%figure(3);
+%imagesc(abs(beamarr));
+%colormap(jet);
+%colorbar();
 
-dirt_img = fft2(visarr);
-beam_img = fft2(beamarr);
+dirt_img = ifft2(visarr);
+beam_img = ifft2(beamarr);
 
 dirt_img = fftshift(dirt_img);
 beam_img = fftshift(beam_img);
 
 figure(4);
-imagesc(abs(dirt_img));
+imagesc(real(flipud(dirt_img)));
+%imagesc(real(dirt_img));
+axis image;
 colormap(gray);
 colorbar();
 print(gcf, '-dpng', pngname);
 
-figure(5);
-imagesc(abs(beam_img));
-colormap(gray);
-colorbar();
+%figure(5);
+%imagesc(abs(beam_img));
+%colormap(gray);
+%colorbar();
